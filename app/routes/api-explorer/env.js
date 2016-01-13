21cc3259f29cec8c2ed11669f412bd3a5dd4eee5
Alexander Harris <harris.alexk@gmail.com>
2016-01-13 13:36:22 -0500

    minor updates. still stuck on re-rendering swagger-ui component

--- app/routes/api-explorer/env.js
+++ app/routes/api-explorer/env.js
@@ -2,21 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    let model = Ember.Object.extend();
-    model.env = params.env;
+    let model = Ember.Object.create();
+    model.set('env', params.env);
 
     return model;
-  },
-
-  apiHost: Ember.computed('model', function() {
-    let env = model.env;
-    let url;
-    if (env === "production") {
-      url = 'https://opendata.arcgis.com/api/v2/swagger.json';
-    } else {
-      url = `https://opendata${env}.arcgis.com/api/v2/swagger.json`;
-    }
-
-    return url;
-  })
+  }
 });
