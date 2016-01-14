0dd53e5e845c5d5ba472a709290d78272824d55b
Alexander Harris <harris.alexk@gmail.com>
2016-01-14 09:59:41 -0500

    adds ablity to switch between dev, qa, and production swagger

--- app/controllers/api-explorer.js
+++ app/controllers/api-explorer.js
@@ -1,4 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  swaggerUrl: 'https://opendataqa.arcgis.com/api/v2/swagger.json',
+  env: Ember.computed('swaggerUrl', function() {
+    let url = this.get('swaggerUrl');
+    let env;
+
+    if (url.indexOf('opendatadev') !== -1) {
+      env = 'dev';
+    } else if (url.indexOf('opendataqa') !== -1) {
+      env = 'qa';
+    } else if (url.indexOf('opendata') !== -1) {
+      env = 'production';
+    } else {
+      env = 'qa';
+    }
+
+    return env;
+  }),
+  isDev: Ember.computed('env', function() {
+    return this.get('env') === 'dev';
+  }),
+  isQa: Ember.computed('env', function() {
+    return this.get('env') === 'qa';
+  }),
+  isProd: Ember.computed('env', function() {
+    return this.get('env') === 'production';
+  }),
+
+  actions: {
+    updateUrl(newUrl) {
+      this.set('swaggerUrl', newUrl);
+      window.swaggerUi.options.url = newUrl;
+      window.swaggerUi.load();
+    }
+  }
 });
