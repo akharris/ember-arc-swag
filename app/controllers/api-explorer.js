e24dac54481f8c794c8c7b5d740d152d43de6e79
Alexander Harris <harris.alexk@gmail.com>
2016-02-25 17:31:22 -0500

    removes ENV toggle and points swagger-ui at production

--- app/controllers/api-explorer.js
+++ app/controllers/api-explorer.js
@@ -3,43 +3,9 @@ import config from '../config/environment';
 
 export default Ember.Controller.extend({
   swaggerUrl: config.APP.swaggerRoot,
-  env: Ember.computed('swaggerUrl', function() {
-    let url = this.get('swaggerUrl');
-    let env;
 
-    if (url.indexOf('opendatadev') !== -1) {
-      env = 'dev';
-    } else if (url.indexOf('opendataqa') !== -1) {
-      env = 'qa';
-    } else if (url.indexOf('opendata') !== -1) {
-      env = 'production';
-    } else if (url.indexOf('localdata') !== -1) {
-      env = 'local';
-    } else {
-      env = 'qa';
-    }
-
-    return env;
-  }),
-
-  isLocal: Ember.computed('env', function() {
-    return this.get('env') === 'local';
-  }),
-  isDev: Ember.computed('env', function() {
-    return this.get('env') === 'dev';
-  }),
-  isQa: Ember.computed('env', function() {
-    return this.get('env') === 'qa';
-  }),
-  isProd: Ember.computed('env', function() {
-    return this.get('env') === 'production';
-  }),
-
-  actions: {
-    updateUrl(newUrl) {
-      this.set('swaggerUrl', newUrl);
-      window.swaggerUi.options.url = newUrl;
-      window.swaggerUi.load();
-    }
+  init() {
+    this._super(...arguments);
+    console.log(this.get('swaggerUrl'));
   }
 });
